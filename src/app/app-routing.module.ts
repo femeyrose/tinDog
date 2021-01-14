66ce59391a3ddcc20060bc5a3c2467dd157d58da
femey <femeyrose12@gmail.com>
2021-01-14 18:58:21 +0530

    css grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
+import { MosaicComponent } from './mosaic/mosaic.component';
+import { ReviewComponent } from './review/review.component';
+import { TrainingComponent } from './training/training.component';
 
 const routes: Routes = [
   {
     path:'',component: HomeComponent,
+  
+  },
+  {
+    path:'about',component: AboutComponent,
+  
+  },
+  {
+    path:'train',component: TrainingComponent,
+  
+  },
+  {
+    path:'gal',component: GalleryComponent,
+  
+  },
+  {
+    path:'rev',component: ReviewComponent,
+  
+  },
+  {
+    path:'con',component: ContactComponent,
+  
+  },
+  {
+    path:'mos',component: MosaicComponent,
   
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import firebase from 'firebase';
+import { AboutComponent } from './about/about.component';
+import { TrainingComponent } from './training/training.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ReviewComponent } from './review/review.component';
+import { ContactComponent } from './contact/contact.component';
+import { MosaicComponent } from './mosaic/mosaic.component';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDGnzKSPRLd6WmQrzpMsR5pnyQ3pdqH3rE",
+  authDomain: "facebook-55fb8.firebaseapp.com",
+  projectId: "facebook-55fb8",
+  storageBucket: "facebook-55fb8.appspot.com",
+  messagingSenderId: "55199059892",
+  appId: "1:55199059892:web:a0554a864c53fbc411ace1",
+  measurementId: "G-SNCLFXW6V0"
+});
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    TrainingComponent,
+    GalleryComponent,
+    ReviewComponent,
+    ContactComponent,
+    MosaicComponent
   ],
   imports: [
     BrowserModule,
